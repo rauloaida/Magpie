9767acd6d0ba7add1b91d170e90e92ce1fd967f0
Raul <raul.oaida.m@gmail.com>
2022-10-12 01:27:19 -0400

    Initial Commit

---
+++ SwiftUiMapApp/SwiftUiMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiMapAppApp.swift
+//  SwiftUiMapApp
+//
+//  Created by Raul Oaida on 10/12/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
